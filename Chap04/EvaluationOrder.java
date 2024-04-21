daa2c4b6ec440249e1bfce2e01408a52b5000f70
takashin9 <takashi64256@gmail.com>
2024-04-21 11:32:35 +0000

    Chap04のdo-while-forまで

---
+++ Chap04/EvaluationOrder.java
@@ -0,0 +1,8 @@
+class EvaluationOrder {
+    public static void main(String[] args) {
+        int a = 3;
+        int x = (a++) * (2 + a);
+        System.out.println("a = " + a);
+        System.out.println("x = " + x);
+    }
+}
