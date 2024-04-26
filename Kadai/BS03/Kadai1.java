8482f6ad0dd233113578a39da933926864b7e701
takashin9 <takashi64256@gmail.com>
2024-04-26 18:18:39 +0900

    BS03課題終わり

---
+++ Kadai/BS03/Kadai1.java
@@ -0,0 +1,7 @@
+class Kadai1 {
+    public static void main(String[] args) {
+        System.out.println("15からカウントダウンします。");
+
+        for (int x = 15; x >= 0; x--) System.out.println(x);
+    }
+}
