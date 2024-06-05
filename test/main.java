3a20b19e876249d021d35340d88f5d9872879ab0
takashin9 <takashi64256@gmail.com>
2024-06-05 04:17:58 +0000

    Chapter 7 までの内容を保存

---
+++ test/main.java
@@ -0,0 +1,9 @@
+package test;
+public class main {
+    public static void test() {
+        System.out.println("test");
+    }
+    public void lala() {
+        System.out.println("lala");
+    }
+}
