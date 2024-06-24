1325ae600bac2068b14ec43f7baec224e80eed77
takashin9 <takashi64256@gmail.com>
2024-06-24 05:57:08 +0000

    BS07の課題の実装終わり

--- Kadai/BS07/Kadai1.java
+++ Kadai/BS07/Kadai1.java
@@ -1,5 +1,4 @@
-import Human;
-calss Kadai1 {
+class Kadai1 {
     public static void main(String[] args) {
         Human hanano = new Human("華乃", 55.0, 1.70);
         Human emari  = new Human("絵真理", 43.0, 1.53);
@@ -16,5 +15,36 @@ calss Kadai1 {
         System.out.println("----" + aika.getName() + "のBMIと適正体重----");
         System.out.println("BMI : " + aika.calculateBMI());
         System.out.println("適正体重 : " + aika.calculateIdealWeight());
+    }
+}
+class Human {
+    private String name;
+    private double weight;
+    private double height;
+
+    public Human(String name, double weight, double height) {
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    double getWeight() {
+        return weight;
+    }
+
+    double getHeight() {
+        return height;
+    }
+
+    double calculateBMI() {
+        return weight / (height * height);
+    }
+
+    double calculateIdealWeight() {
+        return (height * height) * 22;
     }
 }
